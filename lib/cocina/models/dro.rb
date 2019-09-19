c008fcc3f323b0e890a16834e3b7d4994992c8d7
Justin Coyne <jcoyne@justincoyne.com>
2019-09-19 13:26:32 -0500

    Add required version property

--- lib/cocina/models/dro.rb
+++ lib/cocina/models/dro.rb
@@ -20,6 +20,7 @@ module Cocina
       attribute :externalIdentifier, Types::Strict::String
       attribute :type, Types::Strict::String
       attribute :label, Types::Strict::String
+      attribute :version, Types::Strict::Integer
       attribute :access, Access.default { Access.new }
       attribute :administrative, Administrative.default { Administrative.new }
       attribute :identification, Identification.default { Identification.new }
@@ -29,7 +30,8 @@ module Cocina
         params = {
           externalIdentifier: d['externalIdentifier'],
           type: d['type'],
-          label: d['label']
+          label: d['label'],
+          version: d['version']
         }
         if d['access']
           access = {
