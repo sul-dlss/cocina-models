4d0a9c0db4c36376946576a5f329a9801e79accc
Naomi Dushay <ndushay@stanford.edu>
2020-03-02 18:27:14 -0800

    refactor FileSet object classes to use FileSetAttributes

---
+++ lib/cocina/models/file_set_attributes.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Cocina
+  module Models
+    # attributes common to both FileSet and RequestFileSet
+    # See http://sul-dlss.github.io/cocina-models/maps/Fileset.json
+    module FileSetAttributes
+      def self.included(obj)
+        obj.attribute(:identification, FileSet::Identification.default { FileSet::Identification.new })
+        obj.attribute :label, Types::Strict::String
+        obj.attribute :type, Types::String.enum(*FileSet::TYPES)
+        obj.attribute :version, Types::Coercible::Integer
+      end
+    end
+  end
+end
