0659619acc4d6bf7ba64a4fd845dd151c0aae773
Naomi Dushay <ndushay@stanford.edu>
2020-03-04 12:00:48 -0800

    refactor admin_policy and request_admin_policy specs to use admin_policy_shared_examples

--- spec/cocina/models/admin_policy_spec.rb
+++ spec/cocina/models/admin_policy_spec.rb
@@ -1,195 +1,39 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+load 'spec/cocina/models/admin_policy_shared_examples.rb'
 
 RSpec.describe Cocina::Models::AdminPolicy do
-  subject(:admin_policy) { described_class.new(properties) }
-
-  let(:properties) do
+  let(:type) { Cocina::Models::Vocab.admin_policy }
+  # TODO: Allowing description to be omittable for now (until rolled out to consumers),
+  # but I think it's actually required for every admin policy
+  let(:required_properties) do
     {
       externalIdentifier: 'druid:ab123cd4567',
-      type: type,
       label: 'My admin_policy',
-      version: 3,
-      description: {
-        title: []
-      }
+      type: type,
+      version: 3
     }
   end
-  let(:type) { Cocina::Models::Vocab.admin_policy }
 
-  describe 'model check methods' do
+  it_behaves_like 'it has admin_policy attributes'
+
+  context 'when externalIdentifier is missing' do
+    let(:admin_policy) { described_class.new(required_properties.reject { |k, _v| k == :externalIdentifier }) }
+
+    it 'raises a Dry::Struct::Error' do
+      err_msg = '[Cocina::Models::AdminPolicy.new] :externalIdentifier is missing in Hash input'
+      expect { admin_policy }.to raise_error(Dry::Struct::Error, err_msg)
+    end
+  end
+
+  describe 'Checkable model methods' do
+    subject { described_class.new(required_properties) }
+
     it { is_expected.to be_admin_policy }
     it { is_expected.not_to be_collection }
     it { is_expected.not_to be_dro }
     it { is_expected.not_to be_file }
     it { is_expected.not_to be_file_set }
-  end
-
-  describe 'initialization' do
-    context 'with a minimal set, defined above' do
-      it 'has properties' do
-        expect(admin_policy.externalIdentifier).to eq 'druid:ab123cd4567'
-        expect(admin_policy.type).to eq type
-        expect(admin_policy.label).to eq 'My admin_policy'
-
-        expect(admin_policy.access).to be_kind_of Cocina::Models::AdminPolicy::Access
-      end
-    end
-
-    context 'with a string version' do
-      let(:properties) do
-        {
-          externalIdentifier: 'druid:ab123cd4567',
-          type: type,
-          label: 'My admin_policy',
-          version: '3',
-          description: {
-            title: []
-          }
-        }
-      end
-
-      it 'coerces to integer' do
-        expect(admin_policy.version).to eq 3
-      end
-    end
-
-    context 'with all properties' do
-      let(:properties) do
-        {
-          externalIdentifier: 'druid:ab123cd4567',
-          type: type,
-          label: 'My admin_policy',
-          version: 3,
-          access: {
-          },
-          administrative: {
-            default_object_rights: '<rightsMetadata></rightsMetadata>',
-            registration_workflow: 'wasCrawlPreassemblyWF',
-            hasAdminPolicy: 'druid:mx123cd4567'
-          },
-          description: {
-            title: [
-              {
-                primary: true,
-                titleFull: 'My admin_policy'
-              }
-            ]
-          }
-        }
-      end
-
-      it 'has properties' do
-        expect(admin_policy.externalIdentifier).to eq 'druid:ab123cd4567'
-        expect(admin_policy.type).to eq type
-        expect(admin_policy.label).to eq 'My admin_policy'
-        expect(admin_policy.administrative.hasAdminPolicy).to eq 'druid:mx123cd4567'
-        expect(admin_policy.administrative.default_object_rights).to eq '<rightsMetadata></rightsMetadata>'
-        expect(admin_policy.administrative.registration_workflow).to eq 'wasCrawlPreassemblyWF'
-      end
-    end
-  end
-
-  describe '.from_dynamic' do
-    subject(:admin_policy) { described_class.from_dynamic(properties) }
-
-    context 'with empty subschemas' do
-      let(:properties) do
-        {
-          'externalIdentifier' => 'druid:kv840rx2720',
-          'type' => type,
-          'label' => 'Examination of the memorial of the owners and underwriters ...',
-          'version' => 1,
-          'access' => {},
-          'administrative' => {
-            'default_object_rights' => '<rightsMetadata></rightsMetadata>',
-            'registration_workflow' => 'wasCrawlPreassemblyWF',
-            'hasAdminPolicy' => 'druid:mx123cd4567'
-          },
-          'description' => {
-            'title' => []
-          },
-          'identification' => {},
-          'structural' => {}
-        }
-      end
-
-      it 'has properties' do
-        expect(admin_policy.externalIdentifier).to eq 'druid:kv840rx2720'
-        expect(admin_policy.administrative.hasAdminPolicy).to eq 'druid:mx123cd4567'
-        expect(admin_policy.administrative.default_object_rights).to eq '<rightsMetadata></rightsMetadata>'
-        expect(admin_policy.administrative.registration_workflow).to eq 'wasCrawlPreassemblyWF'
-      end
-    end
-  end
-
-  describe '.from_json' do
-    subject(:admin_policy) { described_class.from_json(json) }
-
-    context 'with a minimal admin_policy' do
-      let(:json) do
-        <<~JSON
-          {
-            "externalIdentifier":"druid:12343234",
-            "type":"#{type}",
-            "label":"my admin_policy",
-            "version": 3,
-            "description": {
-              "title": []
-            }
-          }
-        JSON
-      end
-
-      it 'has the attributes' do
-        expect(admin_policy.attributes).to include(externalIdentifier: 'druid:12343234',
-                                                   label: 'my admin_policy',
-                                                   type: type)
-        expect(admin_policy.access).to be_kind_of Cocina::Models::AdminPolicy::Access
-        expect(admin_policy.administrative).to be_kind_of Cocina::Models::AdminPolicy::Administrative
-        expect(admin_policy.identification).to be_kind_of Cocina::Models::AdminPolicy::Identification
-        expect(admin_policy.structural).to be_kind_of Cocina::Models::AdminPolicy::Structural
-      end
-    end
-
-    context 'with a full admin_policy' do
-      let(:json) do
-        <<~JSON
-          {
-            "externalIdentifier":"druid:12343234",
-            "type":"#{type}",
-            "label":"my admin_policy",
-            "version": 3,
-            "access": {
-            },
-            "administrative": {
-              "default_object_rights":"<rightsMetadata></rightsMetadata>",
-              "registration_workflow":"wasCrawlPreassemblyWF",
-              "hasAdminPolicy":"druid:mx123cd4567"
-            },
-            "description": {
-              "title": [
-                {
-                  "primary": true,
-                  "titleFull":"my admin_policy"
-                }
-              ]
-            }
-          }
-        JSON
-      end
-
-      it 'has the attributes' do
-        expect(admin_policy.attributes).to include(externalIdentifier: 'druid:12343234',
-                                                   label: 'my admin_policy',
-                                                   type: type)
-
-        expect(admin_policy.administrative).to be_kind_of Cocina::Models::AdminPolicy::Administrative
-        expect(admin_policy.administrative.hasAdminPolicy).to eq 'druid:mx123cd4567'
-        expect(admin_policy.description.title.first.attributes).to eq(primary: true,
-                                                                      titleFull: 'my admin_policy')
-      end
-    end
   end
 end
--- spec/cocina/models/request_admin_policy_spec.rb
+++ spec/cocina/models/request_admin_policy_spec.rb
@@ -1,178 +1,26 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+load 'spec/cocina/models/admin_policy_shared_examples.rb'
 
 RSpec.describe Cocina::Models::RequestAdminPolicy do
-  subject(:admin_policy) { described_class.new(properties) }
-
   let(:type) { Cocina::Models::Vocab.admin_policy }
-
-  describe 'initialization' do
-    context 'with a minimal set' do
-      let(:properties) do
-        {
-          type: type,
-          label: 'My admin_policy',
-          version: 3,
-          description: {
-            title: []
-          }
-        }
-      end
-
-      it 'has properties' do
-        expect(admin_policy.type).to eq type
-        expect(admin_policy.label).to eq 'My admin_policy'
-
-        expect(admin_policy.access).to be_kind_of Cocina::Models::AdminPolicy::Access
-      end
-    end
-
-    context 'with a string version' do
-      let(:properties) do
-        {
-          type: type,
-          label: 'My admin_policy',
-          version: '3',
-          description: {
-            title: []
-          }
-        }
-      end
-
-      it 'coerces to integer' do
-        expect(admin_policy.version).to eq 3
-      end
-    end
-
-    context 'with all properties' do
-      let(:properties) do
-        {
-          type: type,
-          label: 'My admin_policy',
-          version: 3,
-          access: {
-          },
-          administrative: {
-            default_object_rights: '<rightsMetadata></rightsMetadata>',
-            registration_workflow: 'wasCrawlPreassemblyWF',
-            hasAdminPolicy: 'druid:mx123cd4567'
-          },
-          description: {
-            title: [
-              {
-                primary: true,
-                titleFull: 'My admin_policy'
-              }
-            ]
-          }
-        }
-      end
-
-      it 'has properties' do
-        expect(admin_policy.type).to eq type
-        expect(admin_policy.label).to eq 'My admin_policy'
-        expect(admin_policy.administrative.hasAdminPolicy).to eq 'druid:mx123cd4567'
-        expect(admin_policy.administrative.default_object_rights).to eq '<rightsMetadata></rightsMetadata>'
-        expect(admin_policy.administrative.registration_workflow).to eq 'wasCrawlPreassemblyWF'
-      end
-    end
+  let(:required_properties) do
+    {
+      type: type,
+      label: 'My admin_policy',
+      version: 3
+    }
   end
 
-  describe '.from_dynamic' do
-    subject(:admin_policy) { described_class.from_dynamic(properties) }
+  it_behaves_like 'it has admin_policy attributes'
 
-    context 'with empty subschemas' do
-      let(:properties) do
-        {
-          'type' => type,
-          'label' => 'Examination of the memorial of the owners and underwriters ...',
-          'version' => 1,
-          'access' => {},
-          'administrative' => {
-            'default_object_rights' => '<rightsMetadata></rightsMetadata>',
-            'registration_workflow' => 'wasCrawlPreassemblyWF',
-            'hasAdminPolicy' => 'druid:mx123cd4567'
-          },
-          'description' => {
-            'title' => []
-          },
-          'identification' => {},
-          'structural' => {}
-        }
-      end
-
-      it 'has properties' do
-        expect(admin_policy.label).to eq 'Examination of the memorial of the owners and underwriters ...'
-        expect(admin_policy.administrative.hasAdminPolicy).to eq 'druid:mx123cd4567'
-        expect(admin_policy.administrative.default_object_rights).to eq '<rightsMetadata></rightsMetadata>'
-        expect(admin_policy.administrative.registration_workflow).to eq 'wasCrawlPreassemblyWF'
-      end
-    end
-  end
-
-  describe '.from_json' do
-    subject(:admin_policy) { described_class.from_json(json) }
-
-    context 'with a minimal admin_policy' do
-      let(:json) do
-        <<~JSON
-          {
-            "type":"#{type}",
-            "label":"my admin_policy",
-            "version": 3,
-            "description": {
-              "title": []
-            }
-          }
-        JSON
-      end
-
-      it 'has the attributes' do
-        expect(admin_policy.attributes).to include(label: 'my admin_policy',
-                                                   type: type)
-        expect(admin_policy.access).to be_kind_of Cocina::Models::AdminPolicy::Access
-        expect(admin_policy.administrative).to be_kind_of Cocina::Models::AdminPolicy::Administrative
-        expect(admin_policy.identification).to be_kind_of Cocina::Models::AdminPolicy::Identification
-        expect(admin_policy.structural).to be_kind_of Cocina::Models::AdminPolicy::Structural
-      end
-    end
-
-    context 'with a full admin_policy' do
-      let(:json) do
-        <<~JSON
-          {
-            "type":"#{type}",
-            "label":"my admin_policy",
-            "version": 3,
-            "access": {
-            },
-            "administrative": {
-              "default_object_rights":"<rightsMetadata></rightsMetadata>",
-              "registration_workflow":"wasCrawlPreassemblyWF",
-              "hasAdminPolicy":"druid:mx123cd4567"
-            },
-            "description": {
-              "title": [
-                {
-                  "primary": true,
-                  "titleFull":"my admin_policy"
-                }
-              ]
-            }
-          }
-        JSON
-      end
-
-      it 'has the attributes' do
-        expect(admin_policy.attributes).to include(label: 'my admin_policy',
-                                                   type: type)
-
-        expect(admin_policy.administrative).to be_kind_of Cocina::Models::AdminPolicy::Administrative
-        expect(admin_policy.administrative.hasAdminPolicy).to eq 'druid:mx123cd4567'
-        expect(admin_policy.description.title.first.attributes).to eq(primary: true,
-                                                                      titleFull: 'my admin_policy')
-      end
-    end
+  it 'does not have Checkable model methods' do
+    item = described_class.new(required_properties)
+    expect(item).not_to respond_to(:admin_policy?)
+    expect(item).not_to respond_to(:collection?)
+    expect(item).not_to respond_to(:dro?)
+    expect(item).not_to respond_to(:file?)
+    expect(item).not_to respond_to(:file_set?)
   end
 end
