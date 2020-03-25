e205173df037813242f2342b9af6b9b7283415dc
Justin Littman <justinlittman@stanford.edu>
2020-03-25 13:28:56 -0400

    Added tests of generator outcomes.

---
+++ spec/cocina/generator/schema_ref_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Cocina::Generator::SchemaRef do
+  # This tests the outcome of running exe/generator generate against openapi.yml.
+  context 'when referenced schema is required' do
+    # AdminPolicy.administrative is a required AdminPolicyAdministrative
+    context 'when provided' do
+      let(:policy) do
+        Cocina::Models::AdminPolicy.new({
+                                          externalIdentifier: 'druid:bc123df4567',
+                                          label: 'My admin policy',
+                                          type: Cocina::Models::Vocab.admin_policy,
+                                          version: 1,
+                                          administrative: {}
+                                        }, false, false)
+      end
+
+      it 'handles required' do
+        expect(policy.administrative).to be_kind_of(Cocina::Models::AdminPolicyAdministrative)
+      end
+    end
+
+    context 'when not provided' do
+      let(:policy) do
+        Cocina::Models::AdminPolicy.new({
+                                          externalIdentifier: 'druid:bc123df4567',
+                                          label: 'My admin policy',
+                                          type: Cocina::Models::Vocab.admin_policy,
+                                          version: 1
+                                        }, false, false)
+      end
+
+      it 'defaults to empty AdminPolicyAdministrative' do
+        expect(policy.administrative).to be_kind_of(Cocina::Models::AdminPolicyAdministrative)
+      end
+    end
+  end
+
+  context 'when referenced schema is not required' do
+    # AdminPolicy.description is an optional Description
+    let(:policy) do
+      Cocina::Models::AdminPolicy.new({
+                                        externalIdentifier: 'druid:bc123df4567',
+                                        label: 'My admin policy',
+                                        type: Cocina::Models::Vocab.admin_policy,
+                                        version: 1
+                                      }, false, false)
+    end
+
+    it 'handles omittable' do
+      expect(policy.description).to be_nil
+    end
+  end
+end
---
+++ spec/cocina/models/vocab_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Cocina::Models::Vocab do
+  it 'provides vocab methods' do
+    expect(described_class.admin_policy).to eq('http://cocina.sul.stanford.edu/models/admin_policy.jsonld')
+    expect(described_class.webarchive_seed).to eq('http://cocina.sul.stanford.edu/models/webarchive-seed.jsonld')
+  end
+
+  it 'handles special 3d case' do
+    expect(described_class.three_dimensional).to eq('http://cocina.sul.stanford.edu/models/3d.jsonld')
+  end
+end
