9bfe502bfcef62371acd344efb286dec7cefdb8b
Justin Littman <justinlittman@stanford.edu>
2020-03-26 10:50:16 -0400

    Added additionalProperties:false to all objects in openapi.
    closes #98

--- lib/cocina/models/event.rb
+++ lib/cocina/models/event.rb
@@ -3,13 +3,13 @@
 module Cocina
   module Models
     class Event < Struct
+      attribute :structuredValue, Types::Strict::Array.of(DescriptiveBasicValue).meta(omittable: true)
       # Description of the event (creation, publication, etc.).
       attribute :type, Types::Strict::String.meta(omittable: true)
       attribute :date, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
       attribute :contributor, Types::Strict::Array.of(Contributor).meta(omittable: true)
       attribute :location, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
       attribute :note, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-      attribute :structuredValue, Types::Strict::Array.of(DescriptiveBasicValue).meta(omittable: true)
     end
   end
 end
