33b9294aecd1efd11bab485d249d23eb548c29b0
Justin Coyne <jcoyne@justincoyne.com>
2020-04-01 11:36:02 -0500

    Add a field for download level access
    Ref https://github.com/sul-dlss/dor-services-app/issues/772

--- lib/cocina/models/dro_access.rb
+++ lib/cocina/models/dro_access.rb
@@ -7,13 +7,16 @@ module Cocina
       # The human readable copyright statement that applies
       # example: Copyright World Trade Organization
       attribute :copyright, Types::Strict::String.meta(omittable: true)
+      attribute :embargo, Embargo.optional.meta(omittable: true)
+      # Download access level. This is used in the transition from Fedora as a way to set a default download level at registration that is copied down to all the files.
+
+      attribute :download, Types::Strict::String.default('none').enum('world', 'stanford', 'location-based', 'none').meta(omittable: true)
       # If access is "location-based", which location should have access. This is used in the transition from Fedora as a way to set a default readLocation at registration that is copied down to all the files.
 
       attribute :readLocation, Types::Strict::String.enum('spec', 'music', 'ars', 'art', 'hoover', 'm&m').meta(omittable: true)
       # The human readable use and reproduction statement that applies
       # example: Property rights reside with the repository. Literary rights reside with the creators of the documents or their heirs. To obtain permission to publish or reproduce, please contact the Public Services Librarian of the Dept. of Special Collections (http://library.stanford.edu/spc).
       attribute :useAndReproductionStatement, Types::Strict::String.meta(omittable: true)
-      attribute :embargo, Embargo.optional.meta(omittable: true)
     end
   end
 end
