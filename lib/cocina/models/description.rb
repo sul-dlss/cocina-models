e7eea7d95591c446f1cad52fa51720e915f8c499
arcadiafalcone <arcadia@stanford.edu>
2020-06-03 11:53:47 -0700

    Description: creates Language schema and adds script property

--- lib/cocina/models/description.rb
+++ lib/cocina/models/description.rb
@@ -7,7 +7,7 @@ module Cocina
       attribute :contributor, Types::Strict::Array.of(Contributor).meta(omittable: true)
       attribute :event, Types::Strict::Array.of(Event).meta(omittable: true)
       attribute :form, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-      attribute :language, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+      attribute :language, Types::Strict::Array.of(Language).meta(omittable: true)
       attribute :note, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
       attribute :identifier, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
       attribute :subject, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
---
+++ lib/cocina/models/language.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Cocina
+  module Models
+    class Language < Struct
+      # String value of the descriptive element.
+      attribute :value, Types::Strict::String.meta(omittable: true)
+      # Type of value provided by the descriptive element.
+      attribute :type, Types::Strict::String.meta(omittable: true)
+      # Status of the descriptive element relative to other instances of the element.
+      attribute :status, Types::Strict::String.meta(omittable: true)
+      # Code value of the descriptive element.
+      attribute :code, Types::Strict::String.meta(omittable: true)
+      # URI value of the descriptive element.
+      attribute :uri, Types::Strict::String.meta(omittable: true)
+      attribute :standard, Types::Strict::Array.of(Types::Strict::String).meta(omittable: true)
+      attribute :encoding, Types::Strict::Array.of(Types::Strict::String).meta(omittable: true)
+      attribute :source, Source.optional.meta(omittable: true)
+      attribute :displayLabel, Types::Strict::String.meta(omittable: true)
+      attribute :qualifier, Types::Strict::String.meta(omittable: true)
+      attribute :note, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+      attribute :structuredValue, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+      attribute :parallelValue, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+      attribute :appliesTo, Types::Strict::Array.of(DescriptiveBasicValue).meta(omittable: true)
+      # Alphabet or other notation used to represent a language or other symbolic system
+      attribute :script, DescriptiveValue.optional.meta(omittable: true)
+    end
+  end
+end
