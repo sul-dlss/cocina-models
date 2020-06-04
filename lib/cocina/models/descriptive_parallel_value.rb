44a63d78c355fcf9a4ea7ce48fabc54d57b7b7e2
arcadiafalcone <arcadia@stanford.edu>
2020-06-04 08:53:31 -0700

    Description: removes unnecessary descriptive_parallel_value model file

--- lib/cocina/models/descriptive_parallel_value.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Cocina
-  module Models
-    class DescriptiveParallelValue < Struct
-      attribute :parallelValue, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-    end
-  end
-end
