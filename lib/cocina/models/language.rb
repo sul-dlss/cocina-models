eb648f1e52cd4a6fb220dccdc67030c31c8cd98d
Justin Coyne <jcoyne@justincoyne.com>
2020-10-16 16:35:44 -0500

    Separate language to its own model
    Previously it was not accepting script as a valid property. Possibly becase it was doing allOf with members that specified additionalProperties: false

--- lib/cocina/models/language.rb
+++ lib/cocina/models/language.rb
@@ -3,30 +3,27 @@
 module Cocina
   module Models
     class Language < Struct
-      attribute :structuredValue, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-      attribute :parallelValue, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-      # String or integer value of the descriptive element.
-      attribute :value, Types::Nominal::Any.meta(omittable: true)
-      # Type of value provided by the descriptive element.
-      attribute :type, Types::Strict::String.meta(omittable: true)
-      # Status of the descriptive element value relative to other instances of the element.
-      attribute :status, Types::Strict::String.meta(omittable: true)
+      attribute :appliesTo, Types::Strict::Array.of(DescriptiveBasicValue).meta(omittable: true)
       # Code value of the descriptive element.
       attribute :code, Types::Strict::String.meta(omittable: true)
-      # URI value of the descriptive element.
-      attribute :uri, Types::Strict::String.meta(omittable: true)
-      attribute :standard, Standard.optional.meta(omittable: true)
-      attribute :encoding, Standard.optional.meta(omittable: true)
-      attribute :identifier, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-      attribute :source, Source.optional.meta(omittable: true)
       # The preferred display label to use for the descriptive element in access systems.
       attribute :displayLabel, Types::Strict::String.meta(omittable: true)
-      # A term providing information about the circumstances of the statement (e.g., approximate dates).
-      attribute :qualifier, Types::Strict::String.meta(omittable: true)
+      attribute :encoding, Standard.optional.meta(omittable: true)
       attribute :note, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-      attribute :valueLanguage, DescriptiveValueLanguage.optional.meta(omittable: true)
-      attribute :appliesTo, Types::Strict::Array.of(DescriptiveBasicValue).meta(omittable: true)
+      attribute :parallelValue, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+      # present for mapping to additional schemas in the future and for consistency but not otherwise used
+      attribute :qualifier, Types::Strict::String.meta(omittable: true)
       attribute :script, DescriptiveValue.optional.meta(omittable: true)
+      attribute :source, Source.optional.meta(omittable: true)
+      # Status of the contributor relative to other parallel contributors (e.g. the primary author among a group of contributors).
+      attribute :status, Types::Strict::String.enum('primary').meta(omittable: true)
+      attribute :standard, Standard.optional.meta(omittable: true)
+      attribute :structuredValue, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+      # URI value of the descriptive element.
+      attribute :uri, Types::Strict::String.meta(omittable: true)
+      # Value of the descriptive element.
+      attribute :value, Types::Strict::String.meta(omittable: true)
+      attribute :valueLanguage, DescriptiveValueLanguage.optional.meta(omittable: true)
     end
   end
 end
