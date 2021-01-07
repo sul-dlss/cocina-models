c42e10f923952c73142d1652dca0b2c60d85af7e
Arcadia Falcone <arcadia@stanford.edu>
2021-01-07 11:10:00 -0800

    Rubocop fixes

--- lib/cocina/models/descriptive_admin_metadata.rb
+++ lib/cocina/models/descriptive_admin_metadata.rb
@@ -3,17 +3,12 @@
 module Cocina
   module Models
     class DescriptiveAdminMetadata < Struct
-
-      
-
       attribute :contributor, Types::Strict::Array.of(Contributor).meta(omittable: true)
-attribute :event, Types::Strict::Array.of(Event).meta(omittable: true)
-attribute :language, Types::Strict::Array.of(Language).meta(omittable: true)
-attribute :note, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-attribute :metadataStandard, Types::Strict::Array.of(Standard).meta(omittable: true)
-attribute :identifier, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-
-      
+      attribute :event, Types::Strict::Array.of(Event).meta(omittable: true)
+      attribute :language, Types::Strict::Array.of(Language).meta(omittable: true)
+      attribute :note, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+      attribute :metadataStandard, Types::Strict::Array.of(Standard).meta(omittable: true)
+      attribute :identifier, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
     end
   end
 end
