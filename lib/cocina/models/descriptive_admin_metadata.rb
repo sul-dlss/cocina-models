edc46cd2e354a6668c0cd0a4aa88303b5f4db334
Arcadia Falcone <arcadia@stanford.edu>
2021-01-07 10:00:55 -0800

    Generate model for DescriptiveAdminMetadata

--- lib/cocina/models/descriptive_admin_metadata.rb
+++ lib/cocina/models/descriptive_admin_metadata.rb
@@ -3,12 +3,17 @@
 module Cocina
   module Models
     class DescriptiveAdminMetadata < Struct
+
+      
+
       attribute :contributor, Types::Strict::Array.of(Contributor).meta(omittable: true)
-      attribute :event, Types::Strict::Array.of(Event).meta(omittable: true)
-      attribute :language, Types::Strict::Array.of(Language).meta(omittable: true)
-      attribute :note, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
-      attribute :standard, Standard.optional.meta(omittable: true)
-      attribute :identifier, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+attribute :event, Types::Strict::Array.of(Event).meta(omittable: true)
+attribute :language, Types::Strict::Array.of(Language).meta(omittable: true)
+attribute :note, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+attribute :metadataStandard, Types::Strict::Array.of(Standard).meta(omittable: true)
+attribute :identifier, Types::Strict::Array.of(DescriptiveValue).meta(omittable: true)
+
+      
     end
   end
 end
