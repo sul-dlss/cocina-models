158af010b96513a332dcaa4d42afc3b9cac01bf1
Justin Coyne <jcoyne@justincoyne.com>
2021-03-04 10:35:34 -0600

    Previously we were using the registrationWorkflow field for what should have been disseminationWorkflow
    This adds the second field as well

--- lib/cocina/models/admin_policy_administrative.rb
+++ lib/cocina/models/admin_policy_administrative.rb
@@ -5,6 +5,9 @@ module Cocina
     class AdminPolicyAdministrative < Struct
       attribute :defaultObjectRights, Types::Strict::String.default('<?xml version="1.0" encoding="UTF-8"?><rightsMetadata><access type="discover"><machine><world/></machine></access><access type="read"><machine><world/></machine></access><use><human type="useAndReproduction"/><human type="creativeCommons"/><machine type="creativeCommons" uri=""/><human type="openDataCommons"/><machine type="openDataCommons" uri=""/></use><copyright><human/></copyright></rightsMetadata>').meta(omittable: true)
       attribute :registrationWorkflow, Types::Strict::Array.of(Types::Strict::String).meta(omittable: true)
+      # An additional workflow to start for objects managed by this admin policy once the end-accession workflow step is complete
+      # example: wasCrawlPreassemblyWF
+      attribute :disseminationWorkflow, Types::Strict::String.meta(omittable: true)
       attribute :collectionsForRegistration, Types::Strict::Array.of(Types::Strict::String).meta(omittable: true)
       attribute :roles, Types::Strict::Array.of(AccessRole).meta(omittable: true)
       attribute :hasAdminPolicy, Types::Strict::String
