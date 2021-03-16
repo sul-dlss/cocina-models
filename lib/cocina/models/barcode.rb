9a98a707a93e1566a564a2a004eaee14cb93e4de
Aaron Collier <aaron.collier@stanford.edu>
2021-03-16 16:14:44 -0700

    Validation known barcode formats

--- lib/cocina/models/barcode.rb
+++ lib/cocina/models/barcode.rb
@@ -2,8 +2,11 @@
 
 module Cocina
   module Models
+    # Validates known formats of:
+    #   36105111111111 (Stanford specific barcodes)
+    #   1111-1111 (catkey-copynumber)
     Barcode = Types::String.constrained(
-      format: /^[0-9]{12,20}$/i
+      format: /^[0-9]+-[0-9]+$|^36105[0-9]{9}$/i
     )
   end
 end
