c6c73f7122a7446af95b5f772954a1002cbfa6e3
Michael J. Giarlo <mjgiarlo@stanford.edu>
2021-03-30 08:29:25 -0700

    Copy copyright property from DROAccess to Access (used by collections)
    This supports the work of sul-dlss/argo#2404.

--- lib/cocina/models/access.rb
+++ lib/cocina/models/access.rb
@@ -5,6 +5,9 @@ module Cocina
     class Access < Struct
       # Access level
       attribute :access, Types::Strict::String.default('dark').enum('world', 'stanford', 'location-based', 'citation-only', 'dark').meta(omittable: true)
+      # The human readable copyright statement that applies
+      # example: Copyright World Trade Organization
+      attribute :copyright, Types::Strict::String.meta(omittable: true)
       # If access is "location-based", which location should have access.
       attribute :readLocation, Types::Strict::String.enum('spec', 'music', 'ars', 'art', 'hoover', 'm&m').meta(omittable: true)
       # The human readable use and reproduction statement that applies
