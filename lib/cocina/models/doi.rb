fb94c303d934cfb824e809c205f46d07aea5da31
Justin Coyne <jcoyne@justincoyne.com>
2021-07-08 13:04:05 -0500

    Adapt the DOI pattern to omit the druid namespace
    This follows our existing dois: 10.25740/xd282zn6829 for example

--- lib/cocina/models/doi.rb
+++ lib/cocina/models/doi.rb
@@ -3,7 +3,7 @@
 module Cocina
   module Models
     DOI = Types::String.constrained(
-      format: %r{^10\.25740/druid:[b-df-hjkmnp-tv-z]{2}[0-9]{3}[b-df-hjkmnp-tv-z]{2}[0-9]{4}$}i
+      format: %r{^10\.25740/[b-df-hjkmnp-tv-z]{2}[0-9]{3}[b-df-hjkmnp-tv-z]{2}[0-9]{4}$}i
     )
   end
 end
