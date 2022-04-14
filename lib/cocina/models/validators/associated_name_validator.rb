d5a5aee07576a5bb5823536a89d488d6b80217a8
Justin Littman <justinlittman@stanford.edu>
2022-04-14 13:23:11 -0400

    Add validation of associated names. Adds NameTitleGroupBuilder. Moves builds into own module.
    closes #391

---
+++ lib/cocina/models/validators/associated_name_validator.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+module Cocina
+  module Models
+    module Validators
+      # Validates that when title.note with type "associated name" has a value, it must match a contributor name.
+      class AssociatedNameValidator
+        def self.validate(clazz, attributes)
+          new(clazz, attributes).validate
+        end
+
+        def initialize(clazz, attributes)
+          @clazz = clazz
+          @attributes = attributes.deep_symbolize_keys
+          @error_paths = []
+        end
+
+        def validate
+          return unless meets_preconditions?
+
+          return if resources.all? { |resource| valid?(resource) }
+
+          raise ValidationError,
+                'Missing data: Name associated with uniform title does not match any contributor.'
+        end
+
+        private
+
+        attr_reader :clazz, :attributes, :error_paths
+
+        def meets_preconditions?
+          resources.any? do |resource|
+            titles_with_associated_name_note_for(resource).present?
+          end
+        end
+
+        def valid?(resource)
+          titles_with_associated_name_note_for(resource).all? do |title|
+            contributor_name_value_slices = Builders::NameTitleGroupBuilder
+                                            .build_title_values_to_contributor_name_values(
+                                              Cocina::Models::Title.new(title)
+                                            ).values
+            contributor_name_value_slices.all? do |contributor_name_value_slice|
+              contributors = Array(resource[:contributor]).map do |contributor|
+                Cocina::Models::Contributor.new(contributor)
+              end
+              Builders::NameTitleGroupBuilder.contributor_for_contributor_name_value_slice(
+                contributor_name_value_slice: contributor_name_value_slice, contributors: contributors
+              ).present?
+            end
+          end
+        end
+
+        def resources
+          @resources ||= [description_attributes] + Array(description_attributes[:relatedResource])
+        end
+
+        def description_attributes
+          @description_attributes ||= if [Cocina::Models::Description,
+                                          Cocina::Models::RequestDescription].include?(clazz)
+                                        attributes
+                                      else
+                                        attributes[:description] || {}
+                                      end
+        end
+
+        def associated_name_note_for(title)
+          Array(title[:note]).detect { |note| note[:type] == 'associated name' }
+        end
+
+        def titles_with_associated_name_note_for(resource)
+          Array(resource[:title]).select { |title| associated_name_note_for(title).present? }
+        end
+      end
+    end
+  end
+end
---
+++ spec/cocina/models/validators/associated_name_validator_spec.rb
@@ -0,0 +1,173 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Cocina::Models::Validators::AssociatedNameValidator do
+  let(:validate) { described_class.validate(clazz, props) }
+
+  let(:clazz) { Cocina::Models::Description }
+
+  let(:desc_props) do
+    {
+      title: [
+        {
+          value: 'A title',
+          type: 'uniform',
+          note: [
+            {
+              value: 'Smith, John',
+              type: 'associated name'
+            }
+          ]
+        }
+      ],
+      contributor: [
+        {
+          name: [
+            {
+              value: contributor_name
+            }
+          ]
+        }
+      ]
+    }
+  end
+
+  let(:props) { desc_props }
+
+  let(:contributor_name) { 'Smith, John' }
+
+  context 'when no description' do
+    let(:props) { {} }
+
+    it 'does not raise' do
+      validate
+    end
+  end
+
+  context 'when no associated name' do
+    let(:props) do
+      {
+        title: [
+          {
+            value: 'A title',
+            type: 'uniform',
+            note: [
+              {
+                value: 'Smith, John',
+                type: 'not an associated name'
+              }
+            ]
+          }
+        ],
+        relatedResource: [
+          {
+            title: [
+              {
+                value: 'A title',
+                type: 'uniform'
+              }
+            ]
+          }
+        ]
+      }
+    end
+
+    it 'does not raise' do
+      validate
+    end
+  end
+
+  context 'when valid Description with name title group' do
+    it 'does not raise' do
+      validate
+    end
+  end
+
+  context 'when invalid Description with name title group' do
+    let(:contributor_name) { 'not Smith, John' }
+
+    it 'raises' do
+      expect do
+        validate
+      end.to raise_error(Cocina::Models::ValidationError,
+                         'Missing data: Name associated with uniform title does not match any contributor.')
+    end
+  end
+
+  context 'when invalid Description with missing contributors' do
+    let(:props) do
+      props = desc_props.dup
+      props.delete(:contributor)
+      props
+    end
+
+    it 'raises' do
+      expect do
+        validate
+      end.to raise_error(Cocina::Models::ValidationError,
+                         'Missing data: Name associated with uniform title does not match any contributor.')
+    end
+  end
+
+  context 'when valid Description with related resource' do
+    let(:props) { { relatedResource: [desc_props] } }
+
+    it 'does not raise' do
+      validate
+    end
+  end
+
+  context 'when invalid Description with related resource' do
+    let(:props) { { relatedResource: [desc_props] } }
+
+    let(:contributor_name) { 'not Smith, John' }
+
+    it 'raises' do
+      expect do
+        validate
+      end.to raise_error(Cocina::Models::ValidationError,
+                         'Missing data: Name associated with uniform title does not match any contributor.')
+    end
+  end
+
+  context 'when valid RequestDescription with name title group' do
+    let(:clazz) { Cocina::Models::RequestDescription }
+
+    it 'does not raise' do
+      validate
+    end
+  end
+
+  context 'when invalid RequestDescription with name title group' do
+    let(:clazz) { Cocina::Models::RequestDescription }
+
+    let(:contributor_name) { 'not Smith, John' }
+
+    it 'raises' do
+      expect { validate }.to raise_error(Cocina::Models::ValidationError)
+    end
+  end
+
+  context 'when valid DRO with name title group' do
+    let(:clazz) { Cocina::Models::DRO }
+
+    let(:props) { { description: desc_props } }
+
+    it 'does not raise' do
+      validate
+    end
+  end
+
+  context 'when invalid DRO with name title group' do
+    let(:clazz) { Cocina::Models::DRO }
+
+    let(:contributor_name) { 'not Smith, John' }
+
+    let(:props) { { description: desc_props } }
+
+    it 'raises' do
+      expect { validate }.to raise_error(Cocina::Models::ValidationError)
+    end
+  end
+end
