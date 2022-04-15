0efd90f4a28ab387684ca835ce395e0d1f982914
Justin Littman <justinlittman@stanford.edu>
2022-04-15 12:26:37 -0400

    Handle mixed hashes when validating.

--- lib/cocina/models/validators/validator.rb
+++ lib/cocina/models/validators/validator.rb
@@ -14,8 +14,32 @@ module Cocina
         ].freeze
 
         def self.validate(clazz, attributes)
-          VALIDATORS.each { |validator| validator.validate(clazz, attributes) }
+          # This gets rid of nested model objects.
+          # Once DSA is on Rails 6, this can be:
+          # attributes_hash = attributes.to_h.deep_transform_values do |value|
+          #   value.class.name.starts_with?('Cocina::Models') ? value.to_h : value
+          # end.with_indifferent_access
+          # And add require 'active_support/core_ext/hash/deep_transform_values' to models file.
+
+          # In the meantime, copying code.
+          attributes_hash = deep_transform_values(attributes.to_h) do |value|
+            value.class.name.starts_with?('Cocina::Models') ? value.to_h : value
+          end.with_indifferent_access
+
+          VALIDATORS.each { |validator| validator.validate(clazz, attributes_hash) }
         end
+
+        def self.deep_transform_values(object, &block)
+          case object
+          when Hash
+            object.transform_values { |value| deep_transform_values(value, &block) }
+          when Array
+            object.map { |e| deep_transform_values(e, &block) }
+          else
+            yield(object)
+          end
+        end
+        private_class_method :deep_transform_values
       end
     end
   end
