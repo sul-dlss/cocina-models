3bf062ae9363a2bb7d23b701f21d8c2cae94ad31
Michael J. Giarlo <mjgiarlo@stanford.edu>
2023-03-02 14:55:29 -0800

    Fix a documentation copypasta typo in the OpenAPI spec

--- lib/cocina/models/folio_catalog_link.rb
+++ lib/cocina/models/folio_catalog_link.rb
@@ -7,7 +7,7 @@ module Cocina
       # Catalog that is the source of the linked record.
       # example: folio
       attribute :catalog, Types::Strict::String.enum('folio', 'previous folio')
-      # Only one of the catkeys should be designated for refreshing. This means that this key is the one used to pull metadata from the catalog if there is more than one key present.
+      # Only one of the Folio instance HRIDs should be designated for refreshing. This means that this HRID is the one used to pull metadata from the catalog if there is more than one HRID present.
       attribute :refresh, Types::Strict::Bool.default(false)
       # Record identifier that is unique within the context of the linked record's catalog.
       attribute :catalogRecordId, MigratedFromSymphonyIdentifier | MigratedFromVoyagerIdentifier | CreatedInFolioIdentifier
