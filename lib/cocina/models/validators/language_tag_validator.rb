5ab98756512db11829a010b133f90f8fd2bc035d
Johnathan Martin <john.martin@stanford.edu>
2023-11-08 00:16:08 -0800

    switch LanguageTagValidator to operate on DRO::TYPES so that it's invoked automatically when building item models

--- lib/cocina/models/validators/language_tag_validator.rb
+++ lib/cocina/models/validators/language_tag_validator.rb
@@ -17,10 +17,10 @@ module Cocina
         def validate
           return unless meets_preconditions?
 
-          return if valid_language_tag?
+          return if invalid_files.empty?
 
-          raise ValidationError, 'The provided language tag is not valid according to RFC 4646: ' \
-                                 "#{attributes[:languageTag]}"
+          raise ValidationError, 'Some files have invalid language tags according to RFC 4646: ' \
+                                 "#{invalid_filenames_with_language_tags.join(', ')}"
         end
 
         private
@@ -28,19 +28,45 @@ module Cocina
         attr_reader :clazz, :attributes
 
         def meets_preconditions?
-          file? && attributes[:languageTag].present?
+          dro?
         end
 
-        def file?
-          (clazz::TYPES & File::TYPES).any?
+        def dro?
+          (clazz::TYPES & DRO::TYPES).any?
         rescue NameError
           false
         end
 
-        def valid_language_tag?
-          parsed_tag = I18n::Locale::Tag::Rfc4646.tag(attributes[:languageTag])
+        def valid_language_tag?(file)
+          # I18n::Locale::Tag::Rfc4646.tag will return an instance of I18n::Locale::Tag::Rfc4646 (with fields like language, script,
+          # region) for strings that can be parsed according to RFC 4646, and nil for strings that do not conform to the spec.
+          I18n::Locale::Tag::Rfc4646.tag(file[:languageTag]).present?
+        end
 
-          parsed_tag.present? && parsed_tag.is_a?(I18n::Locale::Tag::Rfc4646)
+        def invalid_files
+          @invalid_files ||= language_tag_files.reject { |file| valid_language_tag?(file) }
+        end
+
+        def invalid_filenames_with_language_tags
+          invalid_files.map { |invalid_file| "#{invalid_file[:filename] || invalid_file[:label]} (#{invalid_file[:languageTag]})" }
+        end
+
+        def language_tag_files
+          files.select { |file| file[:languageTag].present? }
+        end
+
+        def files
+          [].tap do |files|
+            next if attributes.dig(:structural, :contains).nil?
+
+            attributes[:structural][:contains].each do |fileset|
+              next if fileset.dig(:structural, :contains).nil?
+
+              fileset[:structural][:contains].each do |file|
+                files << file
+              end
+            end
+          end
         end
       end
     end
