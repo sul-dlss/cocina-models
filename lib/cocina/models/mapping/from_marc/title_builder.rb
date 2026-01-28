cc31b3faed51f363045ba8c0e1f2e47ee14f0a1e
Justin Coyne <jcoyne@justincoyne.com>
2026-01-28 10:59:01 -0600

    Build titles from MARC

--- lib/cocina/models/mapping/from_marc/title_builder.rb
+++ lib/cocina/models/mapping/from_marc/title_builder.rb
@@ -19,17 +19,28 @@ module Cocina
             @notifier = notifier
           end
 
+          def valid?
+            title_fields = %w[245 246 240 130 740]
+            return true if title_fields.any? { |code| marc[code]&.subfields&.any? }
+
+            notifier.warn('No title fields found')
+            false
+          end
+
           # @return [Hash] a hash that can be mapped to a cocina model
           def build
-            title_fields = %w[245 246 240 130 740]
-            unless title_fields.any? { |code| marc[code]&.subfields&.any? }
-              notifier.warn('No title fields found')
-              return nil
+            return unless valid?
+            return basic_title if has_245a_without_non_sorting?
+
+            if field245
+              parallel_field = linked_field(field245)
+              return parallel_title(parallel_field) if parallel_field
             end
 
-            # Return a basic title
-            # TODO: implement structured values
-            basic_title if basic_title?
+            return alternative_title if alternative_title_field
+            return uniform_title if uniform_title_field
+
+            structured_title(field245)
           end
 
           private
@@ -38,17 +49,78 @@ module Cocina
 
           delegate :fields, to: :marc
 
-          def basic_title?
-            tag = marc['245']
-            tag && tag.indicator2 == '0' && tag.subfields.any? { |subfield| subfield.code == 'a' }
+          def field245
+            @field245 ||= marc['245']
+          end
+
+          def has_245a_without_non_sorting?
+            field245 && field245.indicator2 == '0' && field245.subfields.any? { |subfield| subfield.code == 'a' } &&
+              field245.subfields.none? { |subfield| %w[b f g k n p s].include? subfield.code }
+          end
+
+          def alternative_title_field
+            @alternative_title_field ||= marc['246'] || marc['740']
+          end
+
+          def alternative_title
+            display_label = alternative_title_field.subfields.find { |subfield| subfield.code == 'i' }&.value
+            alt_title = [
+              {
+                value: value_for(alternative_title_field, %w[a b f g k n p s]),
+                displayLabel: display_label,
+                type: 'alternative'
+              }.compact
+            ]
+
+            if (link = linked_field(alternative_title_field))
+              alt_title << { value: value_for(link, %w[a b f g k n p s]), type: 'alternative' }
+            end
+            alt_title
+          end
+
+          def uniform_title_field
+            @uniform_title_field ||= marc['240'] || marc['130']
+          end
+
+          def uniform_title
+            [{
+              value: strip_punctuation(uniform_title_field.select { |subfield| %w[a d f g k l m n o p r s t].include? subfield.code }.map(&:value).join(' ')),
+              type: 'uniform'
+            }]
+          end
+
+          def linked_field(field)
+            pointer = field.subfields.find { |subfield| subfield.code == '6' }
+            return unless pointer
+
+            field_id, index = pointer.value.split('-')
+            marc.fields(field_id)[index.to_i - 1]
           end
 
           def basic_title
-            tag = marc['245']
-
-            title = tag.subfields.find { |subfield| subfield.code == 'a' }
+            title = field245.subfields.find { |subfield| subfield.code == 'a' }
             title_value = strip_punctuation(title.value)
             [{ value: title_value }]
+          end
+
+          def structured_title(field)
+            return unless field
+
+            title = field.subfields.find { |subfield| subfield.code == 'a' }
+            nonsort_count = field.indicator2.to_i
+            non_sort = { value: strip_punctuation(title.value[0..(nonsort_count - 1)]), type: 'nonsorting characters' } unless nonsort_count.zero?
+            sortable = { value: strip_punctuation(title.value[nonsort_count..]), type: 'main title' }
+            subtitle = strip_punctuation(field.select { |subfield| %w[b f g k n p s].include? subfield.code }.map(&:value).join(' '))
+            subtitle_node = { value: subtitle, type: 'subtitle' } if subtitle.present?
+            [{ structuredValue: [non_sort, sortable, subtitle_node].compact }]
+          end
+
+          def parallel_title(linked_field)
+            [{ parallelValue: structured_title(field245) + structured_title(linked_field)}]
+          end
+
+          def value_for(field, subfields)
+            strip_punctuation(field.select { |subfield| subfields.include? subfield.code }.map(&:value).join(' '))
           end
 
           def strip_punctuation(value)
--- spec/cocina/models/mapping/from_marc/title_spec.rb
+++ spec/cocina/models/mapping/from_marc/title_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe Cocina::Models::Mapping::FromMarc::Title do
       end
     end
 
-    context 'with a basic title field' do
+    context 'with a 245a and no non-sorting (ind2)' do
       let(:marc_hash) do
         {
           'fields' => [
@@ -43,6 +43,303 @@ RSpec.describe Cocina::Models::Mapping::FromMarc::Title do
 
       it 'returns the title' do
         expect(build).to eq([{ value: 'Gaudy night' }])
+      end
+    end
+
+    context 'with a 245a and non-sorting (ind2) and no 880' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '245' => { 'ind1' => '1', 'ind2' => '4', 'subfields' => [{ 'a' => 'The unpleasantness at the Bellona Club.' }] } }
+          ]
+        }
+      end
+
+      it 'returns the title' do
+        expect(build).to eq([
+                              {
+                                structuredValue: [
+                                  { value: 'The', type: 'nonsorting characters' },
+                                  { value: 'unpleasantness at the Bellona Club.', type: 'main title' }
+                                ]
+                              }
+                            ])
+      end
+    end
+
+    context 'with title with subtitle (245 abfgknps)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '245' => { 'ind1' => '1', 'ind2' => '4', 'subfields' => [
+              { 'a' => 'The medium term expenditure framework (MTEF) for ... and the annual estimates for ...' },
+              { 'n' => '016,' },
+              { 'p' => 'Ministry of Tourism :' },
+              { 'b' => 'expenditure to be met out of moneys granted and drawn from the consolidated fund, central government budget.' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns the title with structured value' do
+        expect(build).to eq([
+                              {
+                                structuredValue: [
+                                  { value: 'The', type: 'nonsorting characters' },
+                                  { value: 'medium term expenditure framework (MTEF) for ... and the annual estimates for ...', type: 'main title' },
+                                  { value: '016, Ministry of Tourism : expenditure to be met out of moneys granted and drawn from the consolidated fund, central government budget.', type: 'subtitle' }
+                                ]
+                              }
+                            ])
+      end
+    end
+
+    context 'with title with multiple parts (245 abfgknps)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '245' => { 'ind1' => '1', 'ind2' => '0', 'subfields' => [
+              { 'a' => 'Main title :' },
+              { 'b' => 'subtitle,' },
+              { 'f' => 'inclusive dates,' },
+              { 'g' => 'bulk dates,' },
+              { 'k' => 'form,' },
+              { 'n' => 'part name,' },
+              { 'p' => 'part number,' },
+              { 's' => 'version.' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns the title with structured value' do
+        expect(build).to eq([
+                              {
+                                structuredValue: [
+                                  { value: 'Main title', type: 'main title' },
+                                  { value: 'subtitle, inclusive dates, bulk dates, form, part name, part number, version.', type: 'subtitle' }
+                                ]
+                              }
+                            ])
+      end
+    end
+
+    context 'with title with multiple scripts (245/880)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '245' => { 'ind1' => '1', 'ind2' => '0', 'subfields' => [
+              { '6' => '880-01' },
+              { 'a' => 'Publichnai͡a zhenshchina ili publichnai͡a lichnostʹ? :' },
+              { 'b' => 'zhenskie obrazy v kino /' },
+              { 'c' => 'S.A. Smagina.' }
+            ] } },
+            { '880' => { 'ind1' => '1', 'ind2' => '0', 'subfields' => [
+              { '6' => '245-01' },
+              { 'a' => 'Публичная женщина или публичная личность:' },
+              { 'b' => 'женские образы в кино /' },
+              { 'c' => 'С.А. Смагина.' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns the title with parallel values' do
+        expect(build).to eq([
+                              {
+                                parallelValue: [
+                                  {
+                                    structuredValue: [
+                                      { value: 'Publichnai͡a zhenshchina ili publichnai͡a lichnostʹ?', type: 'main title' },
+                                      { value: 'zhenskie obrazy v kino', type: 'subtitle' }
+                                    ]
+                                  },
+                                  {
+                                    structuredValue: [
+                                      { value: 'Публичная женщина или публичная личность', type: 'main title' },
+                                      { value: 'женские образы в кино', type: 'subtitle' }
+                                    ]
+                                  }
+                                ]
+                              }
+                            ])
+      end
+    end
+
+    context 'with alternative title with display label (246)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '246' => { 'ind1' => '1', 'ind2' => ' ', 'subfields' => [
+              { 'i' => 'Title should read:' },
+              { 'a' => 'Valses nobles et sentimentales :' },
+              { 'b' => 'danses,' },
+              { 'f' => '1905,' },
+              { 'g' => 'manuscripts,' },
+              { 'n' => 'op. 5,' },
+              { 'p' => 'L\'oiseau.' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns the alternative title with display label' do
+        expect(build).to eq([
+                              {
+                                value: 'Valses nobles et sentimentales : danses, 1905, manuscripts, op. 5, L\'oiseau.',
+                                displayLabel: 'Title should read:',
+                                type: 'alternative'
+                              }
+                            ])
+      end
+    end
+
+    context 'with uniform title (240)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '240' => { 'ind1' => '1', 'ind2' => '0', 'subfields' => [
+              { 'a' => 'Beowulf.' },
+              { 'l' => 'English' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns the uniform title' do
+        expect(build).to eq([
+                              { value: 'Beowulf. English', type: 'uniform' }
+                            ])
+      end
+    end
+
+    context 'with uniform title with all subfields (240)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '240' => { 'ind1' => '1', 'ind2' => '0', 'subfields' => [
+              { 'a' => 'Uniform title.' },
+              { 'd' => 'Date of treaty,' },
+              { 'f' => 'date of a work,' },
+              { 'g' => 'misc info,' },
+              { 'k' => 'form,' },
+              { 'l' => 'language,' },
+              { 'm' => 'medium of performance,' },
+              { 'n' => 'part number,' },
+              { 'o' => 'arranged music,' },
+              { 'p' => 'part name,' },
+              { 'r' => 'music key,' },
+              { 's' => 'version.' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns the uniform title with all parts' do
+        expect(build).to eq([
+                              {
+                                value: 'Uniform title. Date of treaty, date of a work, misc info, form, language, medium of performance, part number, arranged music, part name, music key, version.',
+                                type: 'uniform'
+                              }
+                            ])
+      end
+    end
+
+    context 'with uniform title (130)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '130' => { 'ind1' => '0', 'ind2' => ' ', 'subfields' => [
+              { 'a' => 'Bible.' },
+              { 'l' => 'Kituba (Congo (Democratic Republic)).' },
+              { 'f' => '1990.' },
+              { '0' => '(SIRSI)4061207' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns the uniform title' do
+        expect(build).to eq([
+                              { value: 'Bible. Kituba (Congo (Democratic Republic)). 1990.', type: 'uniform' }
+                            ])
+      end
+    end
+
+    context 'with uniform title with all subfields (130)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '130' => { 'ind1' => '0', 'ind2' => ' ', 'subfields' => [
+              { 'a' => 'Uniform title.' },
+              { 'd' => 'Date of treaty,' },
+              { 'f' => 'date of a work,' },
+              { 'g' => 'misc info,' },
+              { 'k' => 'form,' },
+              { 'l' => 'language,' },
+              { 'm' => 'medium of performance,' },
+              { 'n' => 'part number,' },
+              { 'o' => 'arranged music,' },
+              { 'p' => 'part name,' },
+              { 'r' => 'music key,' },
+              { 's' => 'version,' },
+              { 't' => 'title.' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns the uniform title with all parts' do
+        expect(build).to eq([
+                              {
+                                value: 'Uniform title. Date of treaty, date of a work, misc info, form, language, medium of performance, part number, arranged music, part name, music key, version, title.',
+                                type: 'uniform'
+                              }
+                            ])
+      end
+    end
+
+    context 'with alternative title (740)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '740' => { 'ind1' => '4', 'ind2' => ' ', 'subfields' => [
+              { 'a' => 'The five red herrings.' },
+              { 'p' => 'Prologue,' },
+              { 'n' => 'chapter 1.' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns the alternative title' do
+        expect(build).to eq([
+                              { value: 'The five red herrings. Prologue, chapter 1.', type: 'alternative' }
+                            ])
+      end
+    end
+
+    context 'with alternative title with multiple scripts (246/880)' do
+      let(:marc_hash) do
+        {
+          'fields' => [
+            { '246' => { 'ind1' => '3', 'ind2' => '0', 'subfields' => [
+              { '6' => '880-01' },
+              { 'a' => 'Nat͡sionalʹnye i mezhdunarodnye proekty razvitii͡a na evraziĭskom prostranstve i perspektivy ikh sopri͡azhenii͡a' }
+            ] } },
+            { '880' => { 'ind1' => '3', 'ind2' => '0', 'subfields' => [
+              { '6' => '246-02' },
+              { 'a' => 'Национальные и международные проекты развития на евразийском пространстве и перспективы их сопряжения' }
+            ] } }
+          ]
+        }
+      end
+
+      it 'returns separate alternative titles' do
+        expect(build).to eq([
+                              { value: 'Nat͡sionalʹnye i mezhdunarodnye proekty razvitii͡a na evraziĭskom prostranstve i perspektivy ikh sopri͡azhenii͡a', type: 'alternative' },
+                              { value: 'Национальные и международные проекты развития на евразийском пространстве и перспективы их сопряжения', type: 'alternative' }
+                            ])
       end
     end
   end
