81da86c955ac54d19693571004ab2a9fc751963c
Justin Coyne <jcoyne@justincoyne.com>
2026-01-29 07:56:18 -0600

    Map languages from MARC

--- lib/cocina/models/mapping/from_marc/description_builder.rb
+++ lib/cocina/models/mapping/from_marc/description_builder.rb
@@ -9,7 +9,7 @@ module Cocina
           BUILDERS = {
             # TODO: implement these builders for MARC
             # note: Note,
-            # language: Language,
+            language: Language
             # contributor: Contributor,
             # event: Event,
             # subject: Subject,
@@ -44,7 +44,7 @@ module Cocina
             cocina_description[:purl] = purl
 
             BUILDERS.each do |description_property, builder|
-              result = builder.build(marc: marc, description_builder: self)
+              result = builder.build(marc: marc)
               cocina_description.merge!(description_property => result) if result.present?
             end
             cocina_description
