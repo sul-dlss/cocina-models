bca9107e31eae87fc221933d5146feff5d0160a2
Justin Coyne <jcoyne@justincoyne.com>
2026-02-11 10:14:45 -0600

    Validate language codes from the MARC data
    We want to drop any invalid languages found in the MARC data.
    
    Fixes #944

--- spec/cocina/models/mapping/from_marc/language_spec.rb
+++ spec/cocina/models/mapping/from_marc/language_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Cocina::Models::Mapping::FromMarc::Language do
       described_class.build(marc: marc)
     end
 
-    let(:require_title) { true }
     let(:marc) { MARC::Record.new_from_hash(marc_hash) }
 
     context 'with multiple duplicate languages' do
@@ -70,8 +69,36 @@ RSpec.describe Cocina::Models::Mapping::FromMarc::Language do
         }
       end
 
-      it 'returns publication event' do
+      it 'returns language list' do
         expect(build).to eq [{ code: 'fre' }, { code: 'ger' }, { code: 'ita'}, { code: 'eng' }]
+      end
+    end
+
+    context 'with invalid languages' do
+      # 008/35-37, 041 $a, $b, $d, $e, $f, $g, $h, $j
+      let(:marc_hash) do
+        {
+          'fields' => [
+            {
+              '008' => '170419p20172015fr opnn  di       n frz d'
+            },
+            {
+              '041' => {
+                'ind1' => '0',
+                'ind2' => ' ',
+                'subfields' => [
+                  {
+                    'd' => 'fre'
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      end
+
+      it 'drops invalid languages' do
+        expect(build).to eq [{ code: 'fre' }]
       end
     end
   end
