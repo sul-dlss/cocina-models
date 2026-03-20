b903575b288da18dbd7b5c1e9a1f82dca2df2854
Laura Wrubel <lwrubel@stanford.edu>
2026-03-20 11:26:36 -0400

    Revert "Merge pull request #992 from sul-dlss/t867-remove-parallel-contributor"
    This reverts commit e4ec4f5599ac9c6e03a14ead961d631589512cf2, reversing
    changes made to f793dda20e6bf01a0c7ffc34f8de5fe7b5d1296e.

--- lib/cocina/models/descriptive_parallel_contributor.rb
+++ lib/cocina/models/descriptive_parallel_contributor.rb
@@ -2,9 +2,10 @@
 
 module Cocina
   module Models
-    # Property model for describing agents contributing in some way to the creation and
-    # history of the resource.
-    class Contributor < Struct
+    # DEPRECATED
+    # Value model for multiple representations of information about the same contributor
+    # (e.g. in different languages).
+    class DescriptiveParallelContributor < Struct
       # Names associated with a contributor.
       attribute :name, Types::Strict::Array.of(DescriptiveValue).default([].freeze)
       # Entity type of the contributor (person, organization, etc.). See https://github.com/sul-dlss/cocina-models/blob/main/docs/description_types.md
@@ -17,15 +18,12 @@ module Cocina
       attribute :role, Types::Strict::Array.of(DescriptiveValue).default([].freeze)
       # Identifiers and URIs associated with the contributor entity.
       attribute :identifier, Types::Strict::Array.of(DescriptiveValue).default([].freeze)
-      # Institutional or other affiliation associated with a contributor.
-      attribute :affiliation, Types::Strict::Array.of(DescriptiveValue).default([].freeze)
       # Other information associated with the contributor.
       attribute :note, Types::Strict::Array.of(DescriptiveValue).default([].freeze)
       # URL or other pointer to the location of the contributor information.
       attribute? :valueAt, Types::Strict::String
-      # For multiple representations of information about the same contributor (e.g. in different
-      # languages).
-      attribute :parallelContributor, Types::Strict::Array.of(DescriptiveParallelContributor).default([].freeze)
+      # Language of the descriptive element value
+      attribute? :valueLanguage, DescriptiveValueLanguage.optional
     end
   end
 end
