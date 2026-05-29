9812eda895f2514543d7ae70012efb336a8706cb
Michael J. Giarlo <mjgiarlo@stanford.edu>
2026-05-29 13:08:28 -0700

    Update barcode format constraint for CHS
    The string should have a fixed length (12).

--- lib/cocina/models/california_historical_society_barcode.rb
+++ lib/cocina/models/california_historical_society_barcode.rb
@@ -4,6 +4,6 @@ module Cocina
   module Models
     # The barcode associated with a CHS DRO, prefixed with 405
     # example: 405000111956
-    CaliforniaHistoricalSocietyBarcode = Types::String.constrained(format: /^405[0-9]+$/)
+    CaliforniaHistoricalSocietyBarcode = Types::String.constrained(format: /^405[0-9]{9}$/)
   end
 end
